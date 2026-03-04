0d802f05d7eb280231af099f6661cd74372b7abf
Bruno Vieira Mendes <bruno.vieira.mendes10@gmail.com>
2026-03-04 01:00:52 -0300

    modulo 11 - Cadastro de Usuarios implementado

---
+++ models/usuario.js
@@ -0,0 +1,40 @@
+const { Schema } = require('mongoose');
+const { cpf } = require('cpf-cnpj-validator');
+
+const UsuarioSchema = new Schema({
+    nome: {
+        type: String,
+        required: true,
+        min: 4,
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return cpf.isValid(v);
+            },
+            message: props => `${props.value} nao e um CPF valido `
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 4,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return v.match('@');
+            },
+            message: props => `${props.value} nao e um e-mail valido` ,
+        },
+    },
+    senha: {
+        type: String,
+        required: true,
+        select: false,
+    },
+});
+
+module.exports = UsuarioSchema;
---
+++ services/cria-usuario.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt');
+
+const { Usuario } = require('../models');
+
+const criaUsuario = async(usuario) => {
+    if (!usuario.senha) {
+        throw new Error('O campo senha e obrigatorio');
+    }
+
+    if (usuario.senha.length <= 4) {
+        throw new Error('O campo senha de ter no minimo 5 caracteres');
+    }
+
+    const hashSenha = await bcrypt.hash(usuario.senha, 10);
+
+    usuario.senha = hashSenha;
+
+    const { senha, ...usuarioSalvo } = (await Usuario.create(usuario))._doc;
+
+    return usuarioSalvo;
+};
+
+module.exports = criaUsuario;
