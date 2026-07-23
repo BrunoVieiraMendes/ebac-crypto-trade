44321f9b0337392f8834d6d65cde753f2bdce4ff
Bruno Vieira Mendes <bruno.vieira.mendes10@gmail.com>
2026-07-23 16:57:03 -0300

    permitindo depositos

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,18 @@
 const { Schema } = require('mongoose');
 const { cpf } = require('cpf-cnpj-validator');
 
+const DepositoSchema = new Schema({
+    valor: {
+        type: Number,
+        required: true,
+        min: 100,
+    },
+    data: {
+        type: Date,
+        require: true,
+    },
+});
+
 const UsuarioSchema = new Schema({
     nome: {
         type: String,
@@ -35,6 +47,7 @@ const UsuarioSchema = new Schema({
         required: true,
         select: false,
     },
+    depositos: [DepositoSchema]
 });
 
 module.exports = UsuarioSchema;
