1afd39a9cf884526b395036701349c02c48fe7ae
Bruno Vieira Mendes <bruno.vieira.mendes10@gmail.com>
2026-09-15 01:03:32 -0300

    corrigindo erro de hidentacao

--- routes/v1/cotacoes.js
+++ routes/v1/cotacoes.js
@@ -6,29 +6,28 @@ const { logger } = require('../../utils');
 const router = express.Router();
 
 /**
-* @openapi
-* /v1/cotacoes:
-*   get:
-*     description: Retorna a última cotação válida de cada moeda no nosso sistema
-*     responses:
-*       200:
-*         description: Recebe uma lista de cotações, atente-se ao id da cotação que será usado depois na troca de moedas
-*          content:
-*             application/json:
-*               schema:
-*                 type: object
-*                 properties:
-*                   sucesso:
-*                     type: boolean
-*                     example: true
-*                   cotacoes:
-*                     type: array
-*                     items: 
-*                          $ref:'#/components/schemas/Cotação'
-*
-*     tags:
-*       - operações
-*/
+ * @openapi
+ * /v1/cotacoes:
+ *   get:
+ *     description: Retorna a última cotação válida de cada moeda no nosso sistema
+ *     responses:
+ *       200:
+ *         description: Recebe uma lista de cotações, atente-se ao id da cotação que será usado depois na troca de moedas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sucesso:
+ *                   type: boolean
+ *                   example: true
+ *                 cotacoes:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Cotacao'
+ *     tags:
+ *       - operacoes
+ */
 
 
 router.get('/', async(_req, res) => {
